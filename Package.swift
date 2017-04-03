7d5da3632232c3b9cd8015e05bc5af342f04277d
Umberto Raimondi <me@uraimo.com>
2017-04-03 21:08:15 +0200

    Updated package name

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "WS281X",
+    name: "WS281x",
     targets: [],
     dependencies: [
         .Package(url: "https://github.com/uraimo/SwiftyGPIO.git",
